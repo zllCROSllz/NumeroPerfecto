fd9df897704e8968a71392eca1187f1f7919d9f3
zllCROSllz <juanpayepes@hotmail.com>
2021-11-08 11:15:02 -0500

    commit

--- src/numeroperfecto/NumeroPerfecto.java
+++ src/numeroperfecto/NumeroPerfecto.java
@@ -12,14 +12,14 @@ public class NumeroPerfecto {
         int rango = entrada.nextInt();
         
         System.out.println("Números perfectos entre "+i+ " y " +rango);
-        for(i=7;i <= rango;i++){      // i es el número que vamos a comprobar
+        for(i=7;i <= rango;i++){      
             suma = 0;
-            for(int j = 1;j < i;j++){    // j son los divisores. Se divide desde 1 hasta i-1                          
+            for(int j = 1;j < i;j++){                             
                  if(i % j==0){
-                    suma = suma + j; // si es divisor se suma
+                    suma = suma + j; 
                  }
             }
-          if(i == suma){             // si el numero es igual a la suma de sus divisores es perfecto              
+          if(i == suma){                          
              System.out.println(i);
           }
         }
